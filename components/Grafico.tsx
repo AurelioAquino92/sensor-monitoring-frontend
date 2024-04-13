166f538e70cd2ccfd47e015f0f47bf0646cb7faa
Aurelio Aquino <aurelio.obmep@gmail.com>
2024-04-13 07:49:26 -0400

    graph color matching navbar

--- components/Grafico.tsx
+++ components/Grafico.tsx
@@ -24,14 +24,14 @@ export default function Grafico({ title, values }: GraficoProps) {
                     <AreaChart data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
                         <defs>
                             <linearGradient id="colorValor" x1="0" y1="0" x2="0" y2="1">
-                                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                                <stop offset="5%" stopColor="#3B82F6" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#3B82F6" stopOpacity={0} />
                             </linearGradient>
                         </defs>
                         <Tooltip />
                         <CartesianGrid />
                         <YAxis domain={['auto', 'auto']} width={30} />
-                        <Area type="monotone" dataKey="valor" stroke="#8884d8" fillOpacity={1} fill="url(#colorValor)" />
+                        <Area type="monotone" dataKey="valor" stroke="#3B82F6" fillOpacity={1} fill="url(#colorValor)" />
                     </AreaChart>
                 </ResponsiveContainer>
             </CardContent>
