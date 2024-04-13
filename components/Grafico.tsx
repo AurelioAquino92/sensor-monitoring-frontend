6845ca7cd414d23949ebce1106385f1b2a5362ff
Aurelio Aquino <aurelio.obmep@gmail.com>
2024-04-13 00:45:26 -0400

    responsiveness and linear area on graph

--- components/Grafico.tsx
+++ components/Grafico.tsx
@@ -3,30 +3,37 @@ import {
     CardContent,
     CardDescription,
     CardHeader,
-  } from "@/components/ui/card"
-import { Bar, BarChart, CartesianGrid, Line, LineChart, Tooltip, YAxis } from "recharts"
-  
+} from "@/components/ui/card"
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, YAxis } from "recharts"
+
 type GraficoProps = {
-  title: string
-  values: number[]
+    title: string
+    values: number[]
 }
-  
-export default function Grafico({title, values} : GraficoProps) {
-    const data = values.map(v => { return {valor: v} })
-    
+
+export default function Grafico({ title, values }: GraficoProps) {
+    const data = values.map(v => { return { valor: v } })
+
     return (
         <Card className="grow overflow-auto">
             <CardHeader className="pb-2">
                 <CardDescription>{title}</CardDescription>
-                {/* <CardTitle className="text-4xl">{dispID}</CardTitle> */}
             </CardHeader>
             <CardContent>
-                <LineChart data={data} height={100} width={200}>
-                    <Tooltip />
-                    <CartesianGrid />
-                    <YAxis domain={['auto', 'auto']}/>
-                    <Line dataKey="valor"/>
-                </LineChart>
+                <ResponsiveContainer height={100} width="100%">
+                    <AreaChart data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
+                        <defs>
+                            <linearGradient id="colorValor" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                            </linearGradient>
+                        </defs>
+                        <Tooltip />
+                        <CartesianGrid />
+                        <YAxis domain={['auto', 'auto']} width={30} />
+                        <Area type="monotone" dataKey="valor" stroke="#8884d8" fillOpacity={1} fill="url(#colorValor)" />
+                    </AreaChart>
+                </ResponsiveContainer>
             </CardContent>
         </Card>
     )
